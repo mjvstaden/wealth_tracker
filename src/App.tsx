396aa4141e1bcec0bd17c40a98e0a7517520c168
MJ van Staden <mjvstaden01@gmail.com>
2025-11-23 16:44:51 +0200

    Fix import case for Layout component
    Update import path to match git-tracked directory casing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
+import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import BuyVsRent from './pages/BuyVsRent';
 import './styles/globals.css';
